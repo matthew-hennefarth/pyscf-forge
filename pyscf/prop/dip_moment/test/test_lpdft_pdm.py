c1e6404932c80184096b14e9f164528369ebdbaa
Helen Clifford <helenclifford@Helens-Air-3.attlocal.net>
2024-08-19 14:29:41 -0500

    finalized the lipdft test cases

--- pyscf/prop/dip_moment/test/test_lpdft_pdm.py
+++ pyscf/prop/dip_moment/test/test_lpdft_pdm.py
@@ -40,30 +40,56 @@ mol_h2o = gto.M(atom = geom_h2o, basis = 'aug-cc-pVDZ', symmetry='c2v', output='
 mol_furan_cation = gto.M(atom = geom_furan, basis = 'sto-3g', charge=1, spin=1, symmetry=False, output='/dev/null', verbose=0)
 
 # Three singlets all of A1 symmetry
-def get_h2o(mol,iroots=3): 
+def get_h2o_ftpbe(mol,iroots=3): 
     weights = [1/iroots]*iroots
     mf = scf.RHF(mol).run()
-    mc = mcpdft.CASSCF(mf,'tPBE', 4, 4, grids_level=1)
-    #mc.fcisolver = csf_solver(mol, smult=1, symm='A1')
+    mc = mcpdft.CASSCF(mf,'ftPBE', 4, 4, grids_level=9)
     fix_spin_(mc.fcisolver, ss=0)
     mc.fcisolver.wfnsym = 'A1'
     mc = mc.multi_state(weights, "lin")
     mo = mcscf.sort_mo(mc, mf.mo_coeff, [4,5,8,9])
-    #mc.conv_tol = 1e-11
+    mc.conv_tol = 1e-12
+    mc.conv_tol_grad = 1e-6
+    mc.kernel(mo)
+    return mc
+
+def get_h2o_ftlda(mol,iroots=3):
+    weights = [1/iroots]*iroots
+    mf = scf.RHF(mol).run()
+    mc = mcpdft.CASSCF(mf,'ftLDA', 4, 4, grids_level=9)
+    fix_spin_(mc.fcisolver, ss=0)
+    mc.fcisolver.wfnsym = 'A1'
+    mc = mc.multi_state(weights, "lin")
+    mo = mcscf.sort_mo(mc, mf.mo_coeff, [4,5,8,9])
+    mc.conv_tol = 1e-12
+    mc.conv_tol_grad = 1e-6
     mc.kernel(mo)
     return mc
 
 # Three doublets of A2, B2, and A2 symmetries
-def get_furan_cation(mol,iroots=3):
+def get_furan_cation_ftpbe(mol,iroots=3):
     weights = [1/iroots]*iroots
     mf = scf.RHF(mol).run()
-    mc = mcpdft.CASSCF(mf,'tBLYP', 5, 5, grids_level=1)
-    #mc.fcisolver = csf_solver(mol, smult=2)
+    mc = mcpdft.CASSCF(mf,'ftPBE', 5, 5, grids_level=9)
     fix_spin_(mc.fcisolver, ss=0.75)
     mc = mc.multi_state(weights, 'lin')
     mc.max_cycle_macro = 200
     mo = mcscf.sort_mo(mc, mf.mo_coeff, [12,17,18,19,20])
-    mc.conv_tol = 1e-11
+    mc.conv_tol = 1e-12
+    mc.conv_tol_grad = 1e-6
+    mc.kernel(mo)
+    return mc
+
+def get_furan_cation_ftlda(mol,iroots=3):
+    weights = [1/iroots]*iroots
+    mf = scf.RHF(mol).run()
+    mc = mcpdft.CASSCF(mf,'ftLDA', 5, 5, grids_level=9)
+    fix_spin_(mc.fcisolver, ss=0.75)
+    mc = mc.multi_state(weights, 'lin')
+    mc.max_cycle_macro = 200
+    mo = mcscf.sort_mo(mc, mf.mo_coeff, [12,17,18,19,20])
+    mc.conv_tol = 1e-12
+    mc.conv_tol_grad = 1e-6
     mc.kernel(mo)
     return mc
 
@@ -80,16 +106,15 @@ class KnownValues(unittest.TestCase):
     The fields were applied along each direction in the XYZ frame, and derivatives 
     were evaluated using 2-point central difference formula.   
     '''
-    def test_h2o_lpdft_tpbe_augccpvdz(self):
+    def test_h2o_lpdft_ftpbe_augccpvdz(self):
         dm_ref = np.array(\
-            [[0.0000, 1.9180, 0.0000],  # State 0: x, y, z
-            [ 0.0000,-1.3233, 0.0000],  # State 1: x, y, z
-            [ 0.0000, 3.2775, 0.0000]]) # State 2: x, y, z
+            [[0.0000, 2.0184, 0.0000],  # State 0: x, y, z
+            [ 0.0000,-1.4674, 0.0000],  # State 1: x, y, z
+            [ 0.0000, 3.3430, 0.0000]]) # State 2: x, y, z
         delta = 0.001
         message = "Dipoles are not equal within {} D".format(delta)
-
         iroots=3
-        mc = get_h2o(mol_h2o, iroots)
+        mc = get_h2o_ftpbe(mol_h2o, iroots)
         for i in range(3):
             with self.subTest (i=i):
                 dm_test = mc.dip_moment(unit='Debye', origin="Coord_center",state=i)
@@ -97,22 +122,55 @@ class KnownValues(unittest.TestCase):
                     self.assertAlmostEqual(dmt, dmr, None, message, delta)
         mc.stdout.close()
         
-#   def test_furan_cation_cms3_tblyp_sto3g(self):
-#       # Numerical ref from this software using 2-point central difference formula
-#       dm_ref = np.array(\
-#           [[0.0000, -1.0146, -1.0418],
-#           [ 0.0000, -1.1001, -1.1296],
-#           [ 0.0000, -0.3954, -0.4061]])
-#       delta = 0.001
-#       message = "Dipoles are not equal within {} D".format(delta)
-#
-#       iroots=3
-#       mc = get_furan_cation(mol_furan_cation, iroots)
-#       for i in range(iroots):
-#           with self.subTest (i=i):
-#               dm_test = mc.dip_moment(unit='Debye', origin="charge_center",state=i)
-#               for dmt,dmr in zip(dm_test,dm_ref[i]):
-#                   self.assertAlmostEqual(dmt, dmr, None, message, delta)
+    def test_h2o_lpdft_ftlda_augccpvdz(self):
+        dm_ref = np.array(\
+            [[0.0000, 1.8875, 0.0000],  # State 0: x, y, z
+            [ 0.0000,-1.4480, 0.0000],  # State 1: x, y, z
+            [ 0.0000, 3.3715, 0.0000]]) # State 2: x, y, z
+        delta = 0.001
+        message = "Dipoles are not equal within {} D".format(delta)
+        iroots=3
+        mc = get_h2o_ftlda(mol_h2o, iroots)
+        for i in range(3):
+            with self.subTest (i=i):
+                dm_test = mc.dip_moment(unit='Debye', origin="Coord_center",state=i)
+                for dmt,dmr in zip(dm_test,dm_ref[i]):
+                    self.assertAlmostEqual(dmt, dmr, None, message, delta)
+        mc.stdout.close()
+
+
+    def test_furan_cation_cms3_ftpbe_sto3g(self):
+        # Numerical ref from this software using 2-point central difference formula
+        dm_ref = np.array(\
+            [[0.0000, -0.8975, -0.9215],
+            [ 0.0000, -1.2513, -1.2847],
+            [ 0.0000, -0.6771, -0.6952]])
+        delta = 0.001
+        message = "Dipoles are not equal within {} D".format(delta)
+        iroots=3
+        mc = get_furan_cation_ftpbe(mol_furan_cation, iroots)
+        for i in range(iroots):
+            with self.subTest (i=i):
+                dm_test = mc.dip_moment(unit='Debye', origin="charge_center",state=i)
+                for dmt,dmr in zip(dm_test,dm_ref[i]):
+                    self.assertAlmostEqual(dmt, dmr, None, message, delta)
+   
+    def test_furan_cation_cms3_ftlda_sto3g(self):
+         # Numerical ref from this software using 2-point central difference formula
+         dm_ref = np.array(\
+             [[0.0000, -0.8332, -0.8555],
+             [ 0.0000, -1.2069, -1.2392],
+             [ 0.0000, -0.6569, -0.6745]])
+         delta = 0.001
+         message = "Dipoles are not equal within {} D".format(delta)
+         iroots=3
+         mc = get_furan_cation_ftlda(mol_furan_cation, iroots)
+         for i in range(iroots):
+             with self.subTest (i=i):
+                 dm_test = mc.dip_moment(unit='Debye', origin="charge_center",state=i)
+                 for dmt,dmr in zip(dm_test,dm_ref[i]):
+                     self.assertAlmostEqual(dmt, dmr, None, message, delta)
+
 
 if __name__ == "__main__":
     print("Test for L-PDFT permanent dipole moments")
